733e70c07204db856ea5eefce710f15e2f9aef86
rosie338 <rosie@corroconsult.com>
2026-09-22 16:37:42 +0100

    Add files via upload

--- Rectifier_app.py
+++ Rectifier_app.py
@@ -47,8 +47,8 @@ left, right = st.columns([2,2])
 with left:
     with st.container(border=True):
         st.subheader("Input Parameters")
-        rhor = st.number_input("Effective Soil Resistivity in Ωcm", value=10000, key="effectivesoilres")
-        Lr = st.number_input("Anode Length in meters", value=12, key="anodelengthr")
+        rhor = st.number_input("Effective Soil Resistivity in Ωcm", value=10000, format="%.3f", key="effectivesoilres")
+        Lr = st.number_input("Anode Length in meters", value=12, format="%.3f",key="anodelengthr")
         dr = st.number_input("Anode Diameter in meters", value=0.203, format="%.3f", key="anodediameterr")
 
 rho, L, d = sp.symbols('rho L d')
